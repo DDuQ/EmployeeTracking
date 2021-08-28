b0ac6a0fa17519db99f2967b6ea063336149827e
Esteban Duque <esteban.duque@masglobalconsulting.com>
2021-08-28 18:52:38 -0500

    Creating table entities

---
+++ EmployeeTracking.Functions/Entities/Consolidated.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace EmployeeTracking.Functions.Entities
+{
+    public class Consolidated : TableEntity
+    {
+        public int employeeId { get; set; }
+        public DateTime date { get; set; }
+        public int minutesWorked { get; set; }
+    }
+}
---
+++ EmployeeTracking.Functions/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace EmployeeTracking.Functions.Entities
+{
+    public class Employee : TableEntity
+    {
+        public int employeeId { get; set; }
+        public DateTime date { get; set; }
+        public int type { get; set; }
+        public bool isConsolidated { get; set; }
+    }
+}
