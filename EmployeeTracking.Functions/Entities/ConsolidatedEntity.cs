a59f78d118717fb62047291427d7a66297a2cc47
Esteban <estebanduque2@gmail.com>
2021-09-02 22:03:50 -0500

    Refactoring entity

--- EmployeeTracking.Functions/Entities/ConsolidatedEntity.cs
+++ EmployeeTracking.Functions/Entities/ConsolidatedEntity.cs
@@ -7,7 +7,6 @@ namespace EmployeeTracking.Functions.Entities
     {
         public int EmployeeId { get; set; }
         public DateTime Date { get; set; }
-        public int Type { get; set; }
-        public bool IsConsolidated { get; set; }
+        public int MinutesWorked { get; set; }
     }
 }
